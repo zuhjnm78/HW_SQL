f354064e3e7bae1b74c1a3bad10431d1d536a712
oleghenkel <oleg.henkel75@gmail.com>
2023-09-14 22:46:47 +0200

    Hw SQL

---
+++ src/main/java/ru/hogwarts/hwsql/HwSqlApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.hwsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hogwarts/hwsql/HwSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.hwsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
